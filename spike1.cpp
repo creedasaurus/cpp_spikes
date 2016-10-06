64a07f7a55192ca6342e003b7c8699adc7fce7b9
Creed Haymond <creedasaurus@gmail.com>
2016-10-06 12:56:32 -0600

    Initial commit. First spike solution for CPP class

---
+++ spike1.cpp
@@ -0,0 +1,31 @@
+#include <string>
+
+
+struct Chinchilla  // ADT abstract Data Type
+{
+	string name;
+	int age;
+
+	string getName() {
+		return name;
+	}
+
+	void display() {
+
+	}
+
+
+
+
+}
+
+
+int main() {
+
+
+
+
+
+
+
+}
