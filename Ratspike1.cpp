2cc7e6f491f8d0a6c70903186e0e44e21b0a171d
Creed Haymond <creedasaurus@gmail.com>
2016-10-15 10:13:50 -0600

    Getting a bin dir, and try to do some stuff with char arrays. Can't remember how to use them inside a struct

--- Ratspike1.cpp
+++ Ratspike1.cpp
@@ -15,36 +15,39 @@ void display(Rat& rat) {
 	cout << rat.age  << endl;
 }
 
-void writeTextData(ofstream& textFile, Rat& rat) {
- 	//TODO write out to textFile the same text and format as display()
-}
+// void writeTextData(ofstream& textFile, Rat& rat) {
+//  	//TODO write out to textFile the same text and format as display()
+// }
 
-void readTextData(ifstream& textFile, Rat& rat) {
-	//TODO read in from textFile the same text and format as display()
-	//TODO write to rat
-}
+// void readTextData(ifstream& textFile, Rat& rat) {
+// 	//TODO read in from textFile the same text and format as display()
+// 	//TODO write to rat
+// }
 
-istream& operator>>(istream& in, Rat& rat) {
-	//TODO read an entire rat from an input stream
-}
+// istream& operator>>(istream& in, Rat& rat) {
+// 	//TODO read an entire rat from an input stream
+// }
 
-ostream& operator<<(ostream& out, Rat& rat) {
-	//TODO write an entire rat to an output stream
-};
+// ostream& operator<<(ostream& out, Rat& rat) {
+// 	//TODO write an entire rat to an output stream
+// };
 
 //----- BINARY I/O -----//
-void writeBinaryData(ofstream& binaryFile, Rat& rat) {
-	//TODO write out to binaryFile the same info in binary format
-}
+// void writeBinaryData(ofstream& binaryFile, Rat& rat) {
+// 	//TODO write out to binaryFile the same info in binary format
+// }
 
-void readBinaryData(ifstream& binaryFile, Rat& rat) {
-	//TODO read in from binaryFile the same info in binary format
-	//TODO write to rat
-}
+// void readBinaryData(ifstream& binaryFile, Rat& rat) {
+// 	//TODO read in from binaryFile the same info in binary format
+// 	//TODO write to rat
+// }
 
 int main() {
 	Rat r1, r2, r3, r4;
 	//TODO r1's name is Ben and age is 2
+	char name1[] = "what";
+	r1.name = name1;
+
 	//TODO r2's name is Willard and age is 3
 	//TODO display r1 & r2
 	//TODO open ofstream rf "ratfarm.txt"
@@ -52,13 +55,15 @@ int main() {
 	//TODO readTextData into r3 & r4
 	//TODO display r3 & r4
 	//TODO open ofstream rf2 "ratfarm2.txt"
-	rf2 << r1;
-	rf2 << r2;
-	rf2 >> r3;
-	rf2 >> r4;
-	cout << r3;
-	cout << r4;
+	// rf2 << r1;
+	// rf2 << r2;
+	// rf2 >> r3;
+	// rf2 >> r4;
+	// cout << r3;
+	// cout << r4;
+	// cout << r1;
 	//TODO read and write binary data in similar vein to above
+	return 0;
 }
 
 
