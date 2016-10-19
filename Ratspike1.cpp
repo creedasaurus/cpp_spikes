1e6cae21f8ff9189299b4104769b71676e8fc117
Creed Haymond <creedasaurus@gmail.com>
2016-10-19 00:10:41 -0600

    Ok. I finished adding the binary portion. And finishing deciding on how to handle the stream objects.

--- Ratspike1.cpp
+++ Ratspike1.cpp
@@ -35,14 +35,17 @@ ostream& operator<<(ostream& out, Rat& rat) {
 };
 
 //----- BINARY I/O -----//
-// void writeBinaryData(ofstream& binaryFile, Rat& rat) {
-// 	//TODO write out to binaryFile the same info in binary format
-// }
+void writeBinaryData(ofstream& binaryFile, Rat& rat) {
+	//TODO write out to binaryFile the same info in binary format
+	// binaryFile << rat.name << '\n' << rat.age << '\n';
+	binaryFile.write(reinterpret_cast<char*>(&rat), sizeof(rat));
+}
 
-// void readBinaryData(ifstream& binaryFile, Rat& rat) {
-// 	//TODO read in from binaryFile the same info in binary format
-// 	//TODO write to rat
-// }
+void readBinaryData(ifstream& binaryFile, Rat& rat) {
+	//TODO read in from binaryFile the same info in binary format
+	binaryFile.read(reinterpret_cast<char*>(&rat), sizeof(rat));
+	//TODO write to rat
+}
 
 int main() {
 	Rat r1, r2, r3, r4;
@@ -54,11 +57,10 @@ int main() {
 	strcpy(r2.name, "Willard");
 	r2.age = 3;
 	
-	
 	//TODO display r1 & r2
 	display(r1);
 	display(r2);
-	
+
 	//TODO open ofstream rf "ratfarm.txt"
 	ofstream rf("ratfarm.txt");
 	
@@ -74,27 +76,20 @@ int main() {
 	readTextData(inf, r4);
 	inf.close();
 	
+
 	//TODO display r3 & r4
     display(r3);	
     display(r4);
     
-    
     cout << " \n----- ratfarm2 -----\n";
 	//TODO open ofstream rf2 "ratfarm2.txt"
 	fstream rf2("ratfarm2.txt", fstream::in | fstream::out | fstream::trunc);
-	if (rf2.is_open()) {
-		cout << "yeehaw" << endl;
-	}
 	
 	rf2 << r1;
 	rf2 << r2;
-// 	rf2.close();
-	
-	
-    // ifstream rf2in("ratfarm2.txt");
+	rf2.seekg(0);
 	rf2 >> r3;
 	rf2 >> r4;
-// 	rf2in.close();
 	
 	
 	cout << r3;
@@ -102,7 +97,32 @@ int main() {
 	cout << r1;
 	rf2.close();
 	
+	cout << "\n~~~ Binary Mode ~~~" << endl;
+
+	Rat rb1, rb2, rb3, rb4;
+	strcpy(rb1.name, "Lando");
+	strcpy(rb2.name, "Buzz");
+	rb1.age = 44;
+	rb2.age = 9;
+
+	display(rb1);
+	display(rb2);
+
+	ofstream rfBin("ratfarm.bin", ios::out | ios::binary);
+
 	//TODO read and write binary data in similar vein to above
+	writeBinaryData(rfBin, rb1);
+	writeBinaryData(rfBin, rb2);
+	rfBin.close();
+
+	ifstream rfBinIN("ratfarm.bin", ios::in | ios::binary);
+	readBinaryData(rfBinIN, rb3);
+	readBinaryData(rfBinIN, rb4);
+
+	display(rb3);
+	display(rb4);
+
+
 	return 0;
 }
 
