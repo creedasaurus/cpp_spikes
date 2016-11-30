878a0b0e389ce5044442478c620fd58d6920dd90
Creed Haymond <creedasaurus@gmail.com>
2016-11-29 17:52:35 -0700

    Testing some sorting times and a partial sort

--- accumulating.cpp
+++ accumulating.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <numeric>
 #include <functional>
+#include <algorithm>
+#include <ctime>
 
 template<class T>
 T add(T a, T b) {
@@ -18,6 +20,26 @@ int main() {
 	std::string first = "James ";
 	std::string last = "Johnson";
 	std::cout << "\n" << add(first, last); 
-	std::cout << "\n" << add(1, 2);
+	std::cout << "\n" << add(1, 2) << std::endl;
+
+	std::vector<int> numbs2;
+	for (int i = 0; i < 100000; i++) {
+		numbs.push_back(rand()%2500+1);
+	}
+	
+	std::clock_t start;
+	std::clock_t now;
+	numbs2 = numbs;
+	start = std::clock();
+	sort(numbs.begin(), numbs.end());
+	now = std::clock();
+	std::cout << "Full Sort: " << now - start << std::endl;
+
+	start = std::clock();
+	partial_sort(numbs2.begin(), numbs2.begin()+1000, numbs2.end());
+	now = std::clock();
+	std::cout << "Partial Sort: " << now - start << std::endl;	
+
+
 	return 0;
 }
